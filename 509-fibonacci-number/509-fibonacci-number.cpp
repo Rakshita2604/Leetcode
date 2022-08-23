9638c3c6d1369a2c5b597f881669a4f3812eedf9
Rakshita <76061570+Rakshita2604@users.noreply.github.com>
2022-08-23 13:56:46 +0530

    Time: 34 ms (5.89%), Space: 5.9 MB (39.80%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -5,13 +5,6 @@ public:
             return 0;
         if(n==1)
             return 1;
-        int a=0,b=1,c=0;
-        for(int i=1;i<n;i++)
-        {
-            c=a+b;
-            a=b;
-            b=c;
-        }
-        return c;
+       return fib(n-1)+fib(n-2);
     }
 };
